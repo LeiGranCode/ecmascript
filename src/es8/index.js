e28d471518d27955c7834998b923a7d410b6cbd9
leigrancode <leiver.g.c@hotmail.es>
2021-04-03 16:40:13 -0500

    Practica con ES8 con objetos y cadenas de texto

---
+++ src/es8/index.js
@@ -0,0 +1,21 @@
+//ES8 Object.entries transformar todos los elementos de Objetos a Arreglos
+const data={
+  frontend: 'Oscar',
+  backend: 'Isabel',
+  Design: 'Ana',
+}
+const arreglo= Object.entries(data);
+console.log(arreglo);
+console.log(arreglo.length);
+//Object values transforma solo los valores de las propiedades de un objeto a Arreglos
+const data={
+  frontend: 'Oscar',
+  backend: 'Isabel',
+  Design: 'Ana', //trading comas, establecer que puede haber o no un elemento próximo
+}
+const valores= Object.values(data);
+console.log(valores);
+//Padding añadir caracteres al inicio y al final de una cadena de texto
+const string ='hello';
+console.log(string.padStart(7,'hi'));
+console.log(string.padStart(12,'-----'));
