914b1b7a4263e265dd32eb038e785b173a0ea9ba
leigrancode <leiver.g.c@hotmail.es>
2021-04-03 16:19:05 -0500

    Practica con ECMAS7

---
+++ src/es7/index.js
@@ -0,0 +1,12 @@
+//ES7
+let numeros=[1,2,6,6,8,7];
+if(numeros.includes(7)){ //validar si hay un valor dentro de un arreglo
+  console.log('Si se encuentra el valor 7')
+}else{
+  console.log('No se ecnuentra.')
+}
+//Potencias con ES7
+let base=4;
+let exponente =3;
+let resul=base ** exponente;
+console.log(resul);
