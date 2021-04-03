6ae20c47692d0043dae66f0aab0240c0c54d25ab
leigrancode <leiver.g.c@hotmail.es>
2021-04-03 15:01:59 -0500

    Segunda practica con arreglos y variables var y let

--- src/sc6/index.js
+++ src/sc6/index.js
@@ -22,3 +22,40 @@ console.log(epicphrase);
 
 let epicPhrase2= `${hello} ${world}`;
 console.log(epicPhrase2);
+
+let lorem=" LALALALAL KDALSKDALSD KASLDKQWJIED QWJKNASDKJ JNKASJNDAjsalk asdjlkasd\n"
++"Otra linea"
+//ES6 templates
+let lorem2 = `otra frase
+ESTA ES OTRA LINEA
+`;
+console.log(lorem);
+console.log(lorem2);
+//objeto y atributos
+let persona={
+  'name': 'oscar',
+  'age': 32,
+  'country': 'MX'
+}
+console.log(persona.name, persona.age);
+//destructurar objeto con ES6
+let {name, age, country}=persona;
+console.log(name,age,country);
+
+//Arreglos - combinar arreglos
+let team1=['Oscar','Julian','Ricardo'];
+let team2=['Valeria','Yesica', 'Camila'];
+let education=['David', ...team1, ...team2];
+console.log(education);
+//variable global y local
+{
+  var globalvar = "variable global";
+}
+{
+  let localvar = "variable local"
+  console.log(localvar);
+}
+console.log(globalvar);
+console.log(localvar);
+//const
+const a='b';
